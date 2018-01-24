8dcfec705262fa54524dc5a331200c4db531ee37
任泽华 <renzholy@gmail.com>
2018-01-24 15:07:11 +0800

    change

--- space.js
+++ space.js
@@ -4,7 +4,7 @@ const baseConfig = require('.');
 const config = Object.assign({}, baseConfig);
 
 config.rules = Object.assign({}, config.rules, {
-	indent: [true, 'spaces', 2]
+  indent: [true, 'spaces', 2]
 });
 
 module.exports = config;
--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@ import test from 'ava';
 import * as tslint from 'tslint';
 
 const runTSLint = filename => {
-	const linter = new tslint.Linter({});
-	const config = tslint.Configuration.findConfiguration('index.js', filename).results;
-	linter.lint(filename, fs.readFileSync(filename, 'utf8'), config);
-	return linter.getResult();
+  const linter = new tslint.Linter({});
+  const config = tslint.Configuration.findConfiguration('index.js', filename).results;
+  linter.lint(filename, fs.readFileSync(filename, 'utf8'), config);
+  return linter.getResult();
 };
 
 test(t => {
-	const result = runTSLint('fixture.ts');
-	t.true(result.failures.some(x => x.ruleName === 'no-empty-interface'));
+  const result = runTSLint('fixture.ts');
+  t.true(result.failures.some(x => x.ruleName === 'no-empty-interface'));
 });
